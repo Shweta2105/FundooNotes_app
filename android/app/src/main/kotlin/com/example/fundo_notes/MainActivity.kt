48f7484876359778b79b29c9d5f4ad418716342d
Shweta2105 <sghadi21@gmail.com>
2021-08-21 14:56:37 +0530

    login, signup and forgot password screen updated

---
+++ android/app/src/main/kotlin/com/example/fundo_notes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fundo_notes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constance/firebaseConfig.dart
@@ -0,0 +1,6 @@
+class Firebase {
+  static String appId = '1:793239451598:android:b0cdac8aca6aca1cba5d9f';
+  static String projectId = 'fundoonotes-31561';
+  static String appiKey = 'AIzaSyB2pWfcTcoLXpqvgyzyk3aY7zYv_8EqYnk';
+  
+}
